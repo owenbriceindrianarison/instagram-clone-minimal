72efeb1287d56785e39610b24143cb8241558360
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-02-14 03:00:23 +0100

    API - CORS

--- instagram-clone-api/main.py
+++ instagram-clone-api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from utils.util import add_tables
 from routers import auth_router, post_router, comment_router
 
@@ -8,6 +9,16 @@ app = FastAPI()
 app.include_router(auth_router.router)
 app.include_router(post_router.router)
 app.include_router(comment_router.router)
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 add_tables()
 
